55331a4088f57d22dda9e5a414fd270a9ec6be06
James <qjameso@berkeley.edu>
2022-04-01 17:40:29 -0700

    set up skeleton frontend

---
+++ client/src/component/Outfit/Outfitcards.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class Outfitcards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="outfitcards">
+        <h1>Outfitcards</h1>
+      </div>
+    );
+  }
+}
+
+export default Outfitcards;
---
+++ client/src/component/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const ProductDetail = (props) => {
+  return (
+    <div className=''>
+      Product Detail
+    </div>
+  );
+}
+
+export default ProductDetail;
+
